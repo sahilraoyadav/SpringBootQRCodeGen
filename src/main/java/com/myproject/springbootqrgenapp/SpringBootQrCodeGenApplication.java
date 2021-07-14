655db44f1f91e38b8003832c726957977491544b
sahilraoyadav <50744190+sahilraoyadav@users.noreply.github.com>
2021-07-14 12:19:24 -0400

    firstCommit

---
+++ src/main/java/com/myproject/springbootqrgenapp/SpringBootQrCodeGenApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.springbootqrgenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQrCodeGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQrCodeGenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/springbootqrgenapp/SpringBootQrCodeGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.springbootqrgenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQrCodeGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
